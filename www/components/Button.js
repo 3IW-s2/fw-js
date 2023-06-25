6046da63724ab89979330c973ec163fc14e0bd9b
Victor GRANDIN <progrdnvictor@gmail.com>
2023-06-25 15:29:30 +0200

    Added various components (some will be removed in the future)

--- www/components/Button.js
+++ www/components/Button.js
@@ -13,19 +13,58 @@
 //  );
 //}
 
-export default function Button({ title, onClick, style }) {
-  const baseStyle = {
-    backgroundColor: "grey",
-    borderRadius: "5px",
-  };
-  return {
-    type: "button",
-    attributes: {
-      style: { ...baseStyle, ...style },
-    },
-    events: {
-      click: onClick,
-    },
-    children: [title],
-  };
+// export default function Button({ title, onClick, style }) {
+//   const baseStyle = {
+//     backgroundColor: "grey",
+//     borderRadius: "5px",
+//   };
+//   return {
+//     type: "button",
+//     attributes: {
+//       style: { ...baseStyle, ...style },
+//     },
+//     events: {
+//       click: onClick,
+//     },
+//     children: [title],
+//   };
+// }
+
+
+import Component from "./Component.js";
+
+class Button extends Component {
+  constructor(props, autoRender = true) {
+    super(props, autoRender);
+  }
+
+  render() {
+    const {
+      title = "Valider",
+      click = {},
+      style = {}
+    } = this.props;
+
+    console.log(this.props);
+
+    const baseStyle = {
+      backgroundColor: "#515151",
+      borderRadius: "4px",
+      color: "#fff",
+      padding: "6px",
+      fontSize: "16px",
+    };
+    return {
+      type: "button",
+      attributes: {
+        style: { ...baseStyle, ...style },
+      },
+      events: {
+        click
+      },
+      children: [title],
+    };
+  }
 }
+
+export default Button;
--- www/components/Compteur.js
+++ www/components/Compteur.js
@@ -1,22 +1,76 @@
-import Button from "./Button.js";
+import Component from "./Component.js";
+import {generateId} from "../core/DomRenderer.js";
+import BrowserLink from "./BrowserLink.js";
+import TitleGenerator from "./TitleGenerator.js";
 
-export default function Compteur({ initialValue = 0 }) {
-  let compteur = initialValue;
+export default class Compteur extends Component {
+  constructor(props, startingValue = 0 ) {
+    super(props);
+    this.state = { counter: startingValue };
+    this.props.title = props?.title ?? "Compteur";
 
-  return {
-    type: "div",
-    children: [
-      Button({
-        title: "-",
-        style: { backgroundColor: "red" },
-        onClick: () => compteur--,
-      }),
-      "Current compteur: {{compteur}}",
-      Button({
-        title: "+",
-        style: { backgroundColor: "green" },
-        onClick: () => compteur++,
-      }),
-    ],
-  };
+    this.titleGenerator = new TitleGenerator(null);
+  }
+
+  increment() {
+    this.setState({ counter: this.state.counter + 1 });
+  }
+
+  decrement() {
+    this.setState({ counter: this.state.counter - 1 });
+  }
+
+  render() {
+    return {
+      type: "div",
+      attributes: {
+        ...this.defaultAttributes,
+      },
+      children: [
+        {
+          type: "button",
+          children: ["-"],
+          events: {
+            click: {
+              handler: () => {
+                this.decrement()
+              },
+            },
+          },
+        },
+        {
+          type: "h2",
+          children: [
+              `${this.props.title}: ${this.state.counter.toString().toUpperCase()}`,
+          ],
+        },
+        {
+          type: "button",
+          children: ["+"],
+          events: {
+            click: {
+              handler: () => {
+                this.increment();
+              },
+            },
+          },
+        },
+        {
+          type: "input",
+          attributes: {
+            type: "text",
+            value: this.state.counter,
+          },
+          events: {
+            keyup: {
+              handler: (event) => {
+                this.setState({ counter: event.target.value });
+              },
+            },
+          },
+        },
+        this.titleGenerator.render()
+      ],
+    };
+  }
 }
